64aa61c71d1f18f486ed20892d4b888f9afcac1c
tprofi <filkabyas@gmail.com>
2019-12-09 11:13:32 +0200

    upd

--- src/com/javalesson/innerclasses/Display.java
+++ src/com/javalesson/innerclasses/Display.java
@@ -6,5 +6,22 @@ public class Display {
     private final int DISPLAY_WIDTH = 1280;
 
     public Display() {
+        Pixel pixel = new Pixel(10, 10, Color.BLUE);
+    }
+
+    public class Pixel {
+        private int x;
+        private int y;
+        Color color;
+
+        public Pixel(int x, int y, Color color) {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
+    }
+
+    public enum Color {
+        RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW, BLACK
     }
 }
