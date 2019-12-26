bef61f733d714c2914f5343be3eb9e4b76a2f2fa
tprofi <filkabyas@gmail.com>
2019-12-27 01:58:48 +0200

    upd

--- src/com/javalesson/exceptions/ExceptionHandligMain.java
+++ src/com/javalesson/exceptions/ExceptionHandligMain.java
@@ -6,22 +6,26 @@ import java.util.Scanner;
 public class ExceptionHandligMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        try {
-            System.out.println("Please enter numerator");
-            int numerator = scanner.nextInt();
-            System.out.println("Please enter dominator");
-            int dominator = scanner.nextInt();
-
-            System.out.println(divide(numerator, dominator));
-        } catch (ArithmeticException e) {
-            System.out.println("Exception: " + e);
-            scanner.nextLine();
-            System.out.println("Enter only non zero parameters allowed");
-        } catch (InputMismatchException e){
-            System.out.println("Exception: "+e);
-            scanner.nextLine();
-            System.out.println("Only integer allowed");
-        }
+        boolean continueloop = true;
+        do {
+            try {
+                System.out.println("Please enter numerator");
+                int numerator = scanner.nextInt();
+                System.out.println("Please enter dominator");
+                int dominator = scanner.nextInt();
+                System.out.println("Result is = "+divide(numerator, dominator));
+                continueloop = false;
+            } catch (ArithmeticException e) {
+                System.out.println("Exception: " + e);
+                scanner.nextLine();
+                System.out.println("Enter only non zero parameters allowed");
+            } catch (InputMismatchException e) {
+                System.out.println("Exception: " + e);
+                scanner.nextLine();
+                System.out.println("Only integer allowed");
+            }
+        } while (continueloop);
+        System.out.println("Try-catch block finished");
     }
 
     private static int divide(int numerator, int dominator) {
