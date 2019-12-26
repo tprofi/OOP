0c6bf8fefda0780e396e4e6ea66d6a60bc174d27
tprofi <filkabyas@gmail.com>
2019-12-27 01:46:27 +0200

    upd

--- src/com/javalesson/exceptions/ExceptionHandligMain.java
+++ src/com/javalesson/exceptions/ExceptionHandligMain.java
@@ -1,8 +1,27 @@
 package com.javalesson.exceptions;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ExceptionHandligMain {
     public static void main(String[] args) {
-        System.out.println(divide(10, 0));
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Please enter numerator");
+            int numerator = scanner.nextInt();
+            System.out.println("Please enter dominator");
+            int dominator = scanner.nextInt();
+
+            System.out.println(divide(numerator, dominator));
+        } catch (ArithmeticException e) {
+            System.out.println("Exception: " + e);
+            scanner.nextLine();
+            System.out.println("Enter only non zero parameters allowed");
+        } catch (InputMismatchException e){
+            System.out.println("Exception: "+e);
+            scanner.nextLine();
+            System.out.println("Only integer allowed");
+        }
     }
 
     private static int divide(int numerator, int dominator) {
