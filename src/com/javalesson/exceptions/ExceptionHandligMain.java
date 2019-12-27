2e6a3ef81c36f504a206c71e1ff95217c2c9f8fb
tprofi <filkabyas@gmail.com>
2019-12-27 02:37:28 +0200

    upd

--- src/com/javalesson/exceptions/ExceptionHandligMain.java
+++ src/com/javalesson/exceptions/ExceptionHandligMain.java
@@ -1,5 +1,8 @@
 package com.javalesson.exceptions;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -13,23 +16,24 @@ public class ExceptionHandligMain {
                 int numerator = scanner.nextInt();
                 System.out.println("Please enter dominator");
                 int dominator = scanner.nextInt();
-                System.out.println("Result is = "+divide(numerator, dominator));
+                System.out.println("Result is = " + divide(numerator, dominator));
                 continueloop = false;
-            } catch (ArithmeticException e) {
+            } catch (ArithmeticException | InputMismatchException e) {
                 System.out.println("Exception: " + e);
                 scanner.nextLine();
-                System.out.println("Enter only non zero parameters allowed");
-            } catch (InputMismatchException e) {
-                System.out.println("Exception: " + e);
-                scanner.nextLine();
-                System.out.println("Only integer allowed");
+                System.out.println("Enter only integer non zero parameters allowed");
             }
+            System.out.println("Try-catch block finished");
         } while (continueloop);
-        System.out.println("Try-catch block finished");
     }
 
-    private static int divide(int numerator, int dominator) {
+    private static int divide(int numerator, int dominator) throws ArithmeticException, NullPointerException, InputMismatchException {
         return numerator / dominator;
+    }
+
+    private static void sveToFile(int res) throws IOException {
+        PrintWriter writer = new PrintWriter(new FileWriter("AndreyKek"));
+        writer.println("Result is "+res);
     }
 
 }
