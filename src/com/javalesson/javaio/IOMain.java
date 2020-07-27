951a208cb77dfba64c4b43128ccf7bf5537041d1
tprofi <filkabyas@gmail.com>
2020-07-27 12:54:09 +0300

    upd

--- src/com/javalesson/javaio/IOMain.java
+++ src/com/javalesson/javaio/IOMain.java
@@ -4,28 +4,26 @@ import com.javalesson.treemap.AverageStudentGrade;
 import com.javalesson.treemap.SubjectGrade;
 import com.javalesson.treemap.TreeMapRunner;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.NavigableMap;
 import java.util.Set;
 
 public class IOMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades = TreeMapRunner.createGrades();
-        try {
-            FileWriter fileWriter = new FileWriter("GradeBook.txt");
-            for (AverageStudentGrade gradeKey : grades.keySet()) {
-                fileWriter.write("==========================================\n");
-                fileWriter.write("Student: " + gradeKey.getName() + " Average Grade: " + gradeKey.getAverageGrade() + "\n");
-                for (SubjectGrade grade : grades.get(gradeKey)) {
-                    fileWriter.write("Subject: " + grade.getSubject() + " Grade: " + grade.getGrade() + "\n");
-                }
-            }
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        writeFile(grades);
+    }
 
+    private static void writeFile(NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades) throws IOException {
+        FileWriter fileWriter = new FileWriter("GradeBook.txt");
+        for (AverageStudentGrade gradeKey : grades.keySet()) {
+            fileWriter.write("==========================================\n");
+            fileWriter.write("Student: " + gradeKey.getName() + " Average Grade: " + gradeKey.getAverageGrade() + "\n");
+            for (SubjectGrade grade : grades.get(gradeKey)) {
+                fileWriter.write("Subject: " + grade.getSubject() + " Grade: " + grade.getGrade() + "\n");
+            }
+        }
+        fileWriter.close();
     }
 }
