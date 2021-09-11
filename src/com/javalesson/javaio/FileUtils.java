180d801412065591e7d4031a4819cc2212fbb64d
tprofi <filkabyas@gmail.com>
2021-09-11 09:57:12 +0300

    up

--- src/com/javalesson/javaio/FileUtils.java
+++ src/com/javalesson/javaio/FileUtils.java
@@ -5,6 +5,11 @@ import java.io.File;
 public class FileUtils {
 
 
+    /**
+     * This method print to standard output a file properties
+     *
+     * @param path - is absolute file path
+     */
     void printIOFileDetails(String path) {
         File file = new File(path);
     }
